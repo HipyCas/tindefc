8e49040a41c7c9d913075d38e36599ced961bf61
Hipy Cas <hipycas@gmail.com>
2021-12-02 15:19:32 +0100

    Update header

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,11 @@ setInterval(() => {
 
 <template>
   <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
+    <header
+      class="shadow-xl w-screen flex justify-center py-5"
+      v-if="$route.meta.title"
+    >
       <img :src="tindefc" alt="Logo" class="h-[60px]" />
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1
-          @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
-        >
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
     </header>
     <main>
       <router-view />
